857f7037ea05e21df58493ae29f4bb66005f1aee
dasnishant1999 <dasnishant1999@gmail.com>
2021-02-09 19:53:57 +0530

    profile image added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import "./Nav.css";
+import netflix_profile from "../media/netflix_profile.jpg";
 
 function Nav() {
   const [show, setshow] = useState(false);
@@ -19,17 +20,13 @@ function Nav() {
   }, []);
 
   return (
-    <div className={`nav ${show &&'nav_black'}`}>
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
       />
-      <img
-        className="nav_avatar"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCHdnmEvoEahtsIo4B0BwdMK2PAHzdvIE59Q&usqp=CAU"
-        alt="Avatar Logo"
-      />
+      <img className="nav_avatar" src={netflix_profile} alt="Avatar Logo" />
     </div>
   );
 }
